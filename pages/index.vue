0b81f37290c18c31d8316e845a90f9700a1e8b91
Mickael Riss <mendos.prod@gmail.com>
2024-02-14 17:20:36 -0500

    Commeres

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- <h1>Test</h1>
+    <CldImage
+      src="Commères/Banner/banner-2"
+      width="200"
+      height="200"
+      alt="My Awesome Image"
+     /> -->
+     <h2>Index</h2>
+     <button class="btn btn-primary">Click</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
